0ee11fa6877efeaf817ffab4e4fc84d072f439fd
batz02 <battilori.matte@gmail.com>
2024-06-24 15:48:29 +0200

    first commit

---
+++ MotorMart/MotorMart/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import *
+
+
+def home(request):
+    return render(request, template_name="home.html")
+
+class UserCreateView(CreateView):
+    #form_class = UserCreationForm
+    form_class = CreateUser
+    template_name = "user_create.html"
+    success_url = reverse_lazy("login")
