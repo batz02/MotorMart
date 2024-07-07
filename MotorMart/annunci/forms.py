5e91e3fcfcd6bbb7a18ea3e2dcf9d55f4eeaf66e
batz02 <battilori.matte@gmail.com>
2024-07-07 17:06:48 +0200

    bug fixing

--- MotorMart/annunci/forms.py
+++ MotorMart/annunci/forms.py
@@ -2,8 +2,24 @@ from django import forms
 from .models import Annuncio
 
 class CreaAnnuncio(forms.ModelForm):
+    stato_opzioni = [
+        ('ottimo', 'Ottimo'),
+        ('buono', 'Buono'),
+        ('danneggiato', 'Danneggiato'),
+    ]
+
+    cambio_opzioni = [
+        ('automatico', 'Automatico'),
+        ('manuale', 'Manuale'),
+    ]
+    
+    stato = forms.ChoiceField(label='Stato', choices=stato_opzioni)
+
+    cambio = forms.ChoiceField(label='Cambio', choices=cambio_opzioni)
+
     class Meta:
         model = Annuncio
-        fields = ['marca', 'modello', 'anno', 'prezzo', 'chilometraggio', 'potenza',
-                  'cilindrata', 'stato', 'carburante', 'cambio', 'colore', 'descrizione', 'immagine']
-        
+        fields = [
+            'marca', 'modello', 'anno', 'prezzo', 'chilometraggio', 'potenza',
+            'cilindrata', 'stato', 'carburante', 'cambio', 'colore', 'descrizione', 'immagine'
+        ]
