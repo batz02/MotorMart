995230c83bda33f9c0b79c69e40c8fffd8948908
batz02 <battilori.matte@gmail.com>
2024-09-03 14:54:18 +0200

    version fix

--- MotorMart/annunci/admin.py
+++ MotorMart/annunci/admin.py
@@ -3,7 +3,6 @@ from .models import Annuncio
 
 
 class AnnuncioAdmin(admin.ModelAdmin):
-    
     list_display = ('marca', 'modello', 'anno', 'prezzo', 'chilometraggio', 'potenza', 'cilindrata', 
                     'stato', 'carburante', 'cambio', 'colore', 'utente', 'data_inserimento', 'venduto')
     list_filter = ('stato', 'carburante', 'cambio', 'venduto', 'data_inserimento')
